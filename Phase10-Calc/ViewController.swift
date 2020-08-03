2303fd0cf022cdfa7b00401ba52e6b181c331b94
AhmadKhalil <a.e.kh1998@gmail.com>
2020-08-04 01:57:53 +0200

    Initial Commit

---
+++ Phase10-Calc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Phase10-Calc
+//
+//  Created by Ahmad Khalil on 8/4/20.
+//  Copyright © 2020 Ahmad Khalil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
